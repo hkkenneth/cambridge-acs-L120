998f0a5081013aeac1090806a4e6487eaf19e1c2
hkkenneth <hkkenneth@gmail.com>
2016-01-12 08:54:35 +0000

    Finish elder

--- project/5-elder.R
+++ project/5-elder.R
@@ -1,43 +1,29 @@
 dim(Data.elder)
+# [1] 500  12
+
 summary(Data.elder)
 
 Data.elder=Data.elder[,c("Y", "X7")]
-plot(Data.elder$X7, Data.elder$Y)
-# funnel shape
+plot(Data.elder$X7, Data.elder$Y)  # funnel shape
 
 lm.fit.elder=lm(Y~X7,data=Data.elder)
 par(mfrow=c(2,2))
 plot(lm.fit.elder)
 summary(lm.fit.elder)
-# 0.3359
-
-#lm.fit.elder.2=lm(Y~log(X7),data=Data.elder)
-#par(mfrow=c(2,2))
-#plot(lm.fit.elder.2)
-# It gives a funny shape for Scale-Location
+# Multiple R-squared:  0.3359
 
 lm.fit.log.elder=lm(log(Y)~X7,data=Data.elder)
 par(mfrow=c(2,2))
 plot(lm.fit.log.elder)
 summary(lm.fit.log.elder)
-# 0.3305
+# Multiple R-squared:  0.3305
 
 lm.fit.sqrt.elder=lm(sqrt(Y)~X7,data=Data.elder)
 par(mfrow=c(2,2))
 plot(lm.fit.sqrt.elder)
 summary(lm.fit.sqrt.elder)
-# 0.3373
-
-lm.fit.elder.5=lm(Y~I(X7^2),data=Data.elder)
-par(mfrow=c(2,2))
-plot(lm.fit.elder.5)
-# This seems to get a straight line for residuals vs fitted and scale-location(upward slope)
-summary(lm.fit.elder.5)
-
-# Still only X7^1 is significant...
-lm.fit.elder.6=lm(Y~poly(X7,3),data=Data.elder)
-par(mfrow=c(2,2))
-plot(lm.fit.elder.6)
-summary(lm.fit.elder.6)
-
-#TODO no idea how to fit this yet
+# Multiple R-squared:  0.3373
+# Coefficients:
+# Estimate Std. Error t value Pr(>|t|)    
+# (Intercept)  7.11973    0.06499  109.55   <2e-16 ***
+# X7           0.17570    0.01104   15.92   <2e-16 ***
