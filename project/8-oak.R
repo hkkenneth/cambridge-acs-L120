53409fba7df9f68f2eacb0b29f85c0325b5e89b8
hkkenneth <hkkenneth@gmail.com>
2016-01-12 11:23:02 +0000

    Finish oak

--- project/8-oak.R
+++ project/8-oak.R
@@ -1,16 +1,15 @@
-# ====== oak ======
-
-dim(Data.oak)
 summary(Data.oak)
+
 Data.oak=Data.oak[,c("Y", "X3")]
+dim(Data.oak)
+# [1] 50  2
+
+par(mfrow=c(2,2))
 plot(Data.oak$X3, Data.oak$Y)
-# ...
-plot((Data.oak$X3)^3, Data.oak$Y) # this still shows some non-linearity
-plot((Data.oak$X3)^4, Data.oak$Y) # these are hard to distinguish
+# Non linearity observed, so try different powers
+plot((Data.oak$X3)^3, Data.oak$Y)
+plot((Data.oak$X3)^4, Data.oak$Y)
 plot((Data.oak$X3)^5, Data.oak$Y)
-lm.fit.oak=lm(Y~poly(X3, 8, raw=T), data=Data.oak)
-# no one is significant...
-summary(lm.fit.oak)
 
 library(glmnet)
 set.seed(1)
@@ -18,31 +17,13 @@ Data.oak.xmat=model.matrix(Y~poly(X3, 8, raw=T)+0, Data.oak)
 lasso.fit.oak=cv.glmnet(Data.oak.xmat,Data.oak$Y,alpha=1)
 plot(lasso.fit.oak)
 predict(lasso.fit.oak, s=lasso.fit.oak$lambda.min, type="coefficients")
+# Lasso chooses X3^4 and X3^5
 
-# This gives very different answer
-Data.oak.xmat.2=model.matrix(Y~poly(X3, 8)+0, Data.oak)
-lasso.fit.oak.2=cv.glmnet(Data.oak.xmat.2,Data.oak$Y,alpha=1)
-predict(lasso.fit.oak.2, s=lasso.fit.oak.2$lambda.min, type="coefficients")
-
-library(leaps)
-best.fit=regsubsets(Y~poly(X3,8,raw=T),data=Data.oak)
-summary(best.fit)
-
-# The following makes no sense...
 k=5
-#set.seed(1)
-#folds=sample(1:k, nrow(Data.oak), replace=T)
+set.seed(1)
+folds=sample(1:k, nrow(Data.oak), replace=T)
 folds=rep(c(1:k), nrow(Data.oak)/k)
 cv.errors=matrix(NA, k, 8, dimnames=list(NULL, paste(1:8)))
-
-predict.regsubsets=function(object, newdata, id,...){
-  form=as.formula(object$call[[2]])
-  mat=model.matrix(form, newdata)
-  coefi=coef(object, id=id)
-  xvars=names(coefi)
-  mat[,xvars]%*%coefi
-}
-
 for (j in 1:k) {
   best.fit=regsubsets(Y~poly(X3, 8, raw=T),data=Data.oak[folds!=j,])
   for (i in 1:8) {
@@ -52,12 +33,17 @@ for (j in 1:k) {
 }
 mean.cv.errors=apply(cv.errors, 2, mean)
 which.min(mean.cv.errors)
-# this gives 3
+# 3
 mean.cv.errors
+#         1         2         3         4         5         6         7         8 
+# 21.006542  6.915234  6.328698  6.739557  6.929283  7.092939  7.848067  8.051623
 
+# Using all data to fit
 best.subset.oak=regsubsets(Y~poly(X3, 8, raw=T),data=Data.oak)
 summary(best.subset.oak)
 coefficients(best.subset.oak, id = 3)
+# (Intercept) poly(X3, 8, raw = T)1 poly(X3, 8, raw = T)2 poly(X3, 8, raw = T)4 
+#   -1.415763            -13.167499              7.161567             55.573382
 
 reg.summary=summary(best.subset.oak)
 
@@ -70,15 +56,11 @@ points(2,reg.summary$cp[2], col="red", cex=2, pch=20)
 plot(reg.summary$bic, xlab="Number of Variables", ylab="BIC", type="l")
 points(2,reg.summary$bic[2], col="red", cex=2, pch=20)
 
-reg.summary=summary(best.fit)
-which.max(reg.summary$adjr2)
-which.min(reg.summary$cp)
-which.min(reg.summary$bic)
-
-# raw=T is very important
-
-# TODO what can we say about this?
-
 lm.fit.oak=lm(Y~X3+I(X3^2)+I(X3^4), data=Data.oak)
 summary(lm.fit.oak)
-
+# Coefficients:
+#             Estimate Std. Error t value Pr(>|t|)    
+# (Intercept)   -1.416      1.908  -0.742    0.462    
+# X3           -13.167     12.643  -1.042    0.303    
+# I(X3^2)        7.162     19.538   0.367    0.716    
+# I(X3^4)       55.573     10.190   5.454 1.89e-06 ***
