8d050612987054183d92069519fa30aacadafa73
hkkenneth <hkkenneth@gmail.com>
2016-01-14 14:38:18 +0000

    Last minute code change

--- project/10-yew.R
+++ project/10-yew.R
@@ -1,19 +1,24 @@
-# ====== yew ======
-
-dim(Data.yew)
 summary(Data.yew)
 Data.yew=Data.yew[,c("Y", "X4")]
-summary(Data.yew)
-head(Data.yew)
+
+dim(Data.yew)
+# [1] 50  2
+
 plot(Data.yew$X4, Data.yew$Y)
+
 Data.yew$Y[which(Data.yew$Y>500)]
+# [1] 999 999
 outliers.yew=which(Data.yew$Y>500)
+
 plot(Data.yew$X4[-outliers.yew], Data.yew$Y[-outliers.yew])
-summary(Data.yew$Y[-outliers.yew])
 lm.fit.clean.yew=lm(Y~X4, data=Data.yew[-outliers.yew,])
+lm.fit.raw.yew=lm(Y~X4, data=Data.yew)
 abline(lm.fit.clean.yew)
+abline(lm.fit.raw.yew, col="red")
 cor(Data.yew$X4, Data.yew$Y)
+# [1] -0.1537
 cor(Data.yew$X4[-outliers.yew], Data.yew$Y[-outliers.yew])
+# [1] 0.9733695
 
 plot(Data.yew$X4[-outliers.yew], Data.yew$Y[-outliers.yew])
 lm.fit.raw.yew=lm(Y~X4, data=Data.yew)
@@ -22,22 +27,16 @@ lm.fit.clean1.yew=lm(Y~X4, data=Data.yew[-outliers.yew[1],])
 # the outlier with X4 ~ 0
 lm.fit.clean2.yew=lm(Y~X4, data=Data.yew[-outliers.yew[2],])
 
-
-summary(lm.fit.raw.yew)
-summary(lm.fit.clean1.yew)
-summary(lm.fit.clean2.yew)
-summary(lm.fit.clean.yew)
-
-abline(lm.fit.yew, col="red")
-abline(lm.fit.yew.2, col="blue")
-
 plot(Data.yew$X4, Data.yew$Y)
 abline(lm.fit.raw.yew, col="red")
 abline(lm.fit.clean.yew, col="blue")
 abline(lm.fit.clean1.yew, col="green")
 abline(lm.fit.clean2.yew, col="orange")
-# TODO compare the effect of outliers - quantify it with the use of leverage
 
-# hatvalues
+# hatvalues (leverage points)
 hatvalues(lm.fit.raw.yew)[outliers.yew]
+#        110       1352 
+# 0.02238737 0.07208742
 summary(hatvalues(lm.fit.raw.yew))
+#    Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+# 0.02023 0.02311 0.03412 0.04000 0.05870 0.07567
--- project/9-rowan.R
+++ project/9-rowan.R
@@ -1,21 +1,40 @@
-# ====== rowan ======
-
-dim(Data.rowan)
 summary(Data.rowan)
+
 Data.rowan=Data.rowan[,c("Y", "X4")]
-# these observations do not contain any outliers or high leverage points
+dim(Data.rowan)
+# [1] 50  2
+
 plot(Data.rowan$X4, Data.rowan$Y)
-# close-to-one Pearson correlation coefficient: high correlation between X4 and Y
+# These observations do not contain any outliers or high leverage points
+
 cor(Data.rowan$X4, Data.rowan$Y)
-# simple linear regression
+# [1] 0.9742865
+# close-to-one Pearson correlation coefficient: high correlation between X4 and Y
+
+# Simple linear regression
 lm.fit.rowan=lm(Y~X4, data=Data.rowan)
 summary(lm.fit.rowan)
-abline(lm.fit.rowan)
-# various plots for verification, including residuals vs fitted values
+# Coefficients:
+#             Estimate Std. Error t value Pr(>|t|)    
+# (Intercept)   2.1534     0.5917    3.64 0.000667 ***
+# X4            3.0237     0.1009   29.96  < 2e-16 ***
+# ...
+# Multiple R-squared:  0.9492
+
+abline(lm.fit.rowan, col="red")
+
+# Various plots for verification, including residuals vs fitted values
 par(mfrow=c(2,2))
 plot(lm.fit.rowan)
-# curiosity check
+
+# Curiosity check
 lm.fit.rowan.5=lm(Y~poly(X4,5), data=Data.rowan)
 summary(lm.fit.rowan.5)
-
-# DONE?
+# Coefficients:
+#              Estimate Std. Error t value Pr(>|t|)    
+# (Intercept)  17.24188    0.31127  55.391   <2e-16 ***
+# poly(X4, 5)1 65.77109    2.20104  29.882   <2e-16 ***
+# poly(X4, 5)2  0.49983    2.20104   0.227   0.8214    
+# poly(X4, 5)3 -0.57249    2.20104  -0.260   0.7960    
+# poly(X4, 5)4 -0.09382    2.20104  -0.043   0.9662    
+# poly(X4, 5)5  4.19547    2.20104   1.906   0.0632 .
